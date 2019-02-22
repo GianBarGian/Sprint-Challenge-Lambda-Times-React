9a627c15a8e4d6d8c75c69089e684c3589572ce1
Giacomo Benati <giacomobenati@mailbox.org>
2019-02-22 11:33:26 +0100

    (STRETCH) Tabs refactored to use styled-components

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import Tab from './Tab';
+import styled from 'styled-components';
 import PT from 'prop-types';
+import Tab from './Tab';
+
+const TabsStyled = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+const TopicsStyled = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const TitleStyled = styled.span`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
 
 const Tabs = ({ tabs, selectedTab, selectTabHandler }) => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <TabsStyled>
+      <TopicsStyled>
+        <TitleStyled>TRENDING TOPICS:</TitleStyled>
         {
             tabs.map(tab => 
               <Tab 
@@ -16,8 +45,8 @@ const Tabs = ({ tabs, selectedTab, selectTabHandler }) => {
                 key={tab}
               />)    
         }
-      </div>
-    </div>
+      </TopicsStyled>
+    </TabsStyled>
   );
 };
 
